5b9021da442c997254b0dab4a209fc9fcbdd3541
lukden <dean.l@northeastern.edu>
2022-03-24 15:25:15 -0400

    add update function

--- controllers/businesses.js
+++ controllers/businesses.js
@@ -1,4 +1,5 @@
 import { Business } from "../models/business.js"
+import { v2 as cloudinary } from 'cloudinary'
 
 function index (req, res) {
   Business.find({})
@@ -11,6 +12,42 @@ function index (req, res) {
   })
 }
 
+function update (req, res) {
+  if (req.body.photo === 'undefined' || !req.files['photo']) {
+    delete req.body['photo']
+    Business.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(business => {
+      business.populate('owner')
+      .then(populatedBusiness => {
+        res.status(201).json(populatedBusiness)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  } else {
+    const imageFile = req.files.photo.path
+    cloudinary.uploader.upload(imageFile, {tags: `${req.body.name}`})
+    .then(image => {
+      console.log(image)
+      req.body.photo = image.url
+      Business.findByIdAndUpdate(req.params.id, req.body, {new: true})
+      .then(business => {
+        business.populate('owner')
+        .then(populatedBusiness => {
+          res.status(201).json(populatedBusiness)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+    })
+  }
+}
+
 export {
-  index
+  index,
+  update
 }
