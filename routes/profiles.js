783a811b2e688699adf016a4059cefc7313da691
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-03-28 09:42:08 -0500

    add route to get profile

--- routes/profiles.js
+++ routes/profiles.js
@@ -10,5 +10,6 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
+router.get('/:id', checkAuth, profilesCtrl.show)
 
 export { router }
